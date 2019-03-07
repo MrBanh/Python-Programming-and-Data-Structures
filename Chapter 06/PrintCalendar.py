750532be1069d33031e137337eb8e0cde760e3d6
DESKTOP-0P21BI7\Tony B <tonybanh@live.com>
2019-03-07 03:30:17 -0800

    Chapter 6 function abstraction and stepwise refinement example: PrintCalendar.py --- create the layout of program

---
+++ Chapter 06/PrintCalendar.py
@@ -0,0 +1,58 @@
+'''
+PrintCalendar.py - prints the calendar based on year and month given by user
+
+    Inputs:
+        year: int -- User input the full year (e.g. 2001)
+        day: int -- User input the month as a number between 1 and 12
+
+    Outputs Example:
+        December 2014
+-----------------------------
+ Sun Mon Tue Wed Thu Fri Sat
+       1   2   3   4   5   6
+   7   8   9  10  11  12  13
+  14  15  16  17  18  19  20
+  21  22  23  24  25  26  27
+  28  29  30  31
+'''
+# TODO: Add function type hinting
+
+
+def print_month(year, month):
+    pass
+
+
+def print_month_title(year, month):
+    pass
+
+
+def get_month_name(month):
+    pass
+
+
+def print_month_body(year, month):
+    pass
+
+
+def get_start_day(year, month):
+    pass
+
+
+def get_total_num_of_days(year, month):
+    pass
+
+
+def get_num_of_days_in_month(year, month):
+    pass
+
+
+def is_Leap_Year(year):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
